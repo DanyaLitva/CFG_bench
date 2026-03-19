5e36a590c75e3951bfcb84a7377d1dda2ff1d41b
Litvyakov Daniil <140255125+DanyaLitva@users.noreply.github.com>
2026-03-19 20:01:09 +0300

    а

--- test.c
+++ test.c
@@ -1,3 +1,11 @@
+// Number of benchmark runs on a single graph
+#define COUNT 1
+// If true, the first run is done without measuring time (warm-up)
+#define HOT false
+// Use your custom configuration for the benchmark (default is the xz.g graph
+// and vf.cnf grammar)
+#define configs configs_all
+
 #include <GraphBLAS.h>
 #include <LAGraph.h>
 #include <LAGraphX.h>
@@ -114,7 +122,8 @@ char *configs_vf[] = {"data/graphs/vf/xz.g,data/grammars/vf.cnf",
                       "data/graphs/vf/nab.g,data/grammars/vf.cnf",
                       "data/graphs/vf/leela.g,data/grammars/vf.cnf", NULL};
 
-char *configs_aa[] = {"data/graphs/aa/cactus.g,data/grammars/aa.cnf",
+char *configs_aa[] = {
+                      "data/graphs/aa/cactus.g,data/grammars/aa.cnf",
                       "data/graphs/aa/imagick.g,data/grammars/aa.cnf",
                       "data/graphs/aa/leela.g,data/grammars/aa.cnf",
                       "data/graphs/aa/nab.g,data/grammars/aa.cnf",
@@ -126,68 +135,60 @@ char *configs_aa[] = {"data/graphs/aa/cactus.g,data/grammars/aa.cnf",
                       "data/graphs/aa/xz.g,data/grammars/aa.cnf",
                        NULL};
 
-char *configs_all[] = {"data/graphs/rdf/go_hierarchy.g,data/grammars/"
-                       "nested_parentheses_subClassOf_type.cnf",
-                       "data/graphs/rdf/taxonomy.g,data/grammars/"
-                       "nested_parentheses_subClassOf_type.cnf",
-                       "data/graphs/rdf/eclass.g,data/grammars/"
-                       "nested_parentheses_subClassOf_type.cnf",
-                       "data/graphs/rdf/go.g,data/grammars/"
-                       "nested_parentheses_subClassOf_type.cnf",
-                       "data/graphs/rdf/taxonomy_hierarchy.g,data/grammars/"
-                       "nested_parentheses_subClassOf_type.cnf",
+char *configs_all[] = {
+                        "data/graphs/java/eclipse.g,data/grammars/java_points_to.cnf",
+                        "data/graphs/java/lusearch.g,data/grammars/java_points_to.cnf",
+                        "data/graphs/java/luindex.g,data/grammars/java_points_to.cnf",
+                        "data/graphs/java/commons_io.g,data/grammars/java_points_to.cnf",
+                        "data/graphs/java/sunflow.g,data/grammars/java_points_to.cnf",
 
-                       "data/graphs/java/eclipse.g,data/grammars/java_points_to.cnf",
-    "data/graphs/java/lusearch.g,data/grammars/java_points_to.cnf",
-    "data/graphs/java/luindex.g,data/grammars/java_points_to.cnf",
-    "data/graphs/java/commons_io.g,data/grammars/java_points_to.cnf",
-    "data/graphs/java/sunflow.g,data/grammars/java_points_to.cnf",
+                        "data/graphs/c_alias/init.g,data/grammars/c_alias.cnf",
+                        "data/graphs/c_alias/block.g,data/grammars/c_alias.cnf",
+                        "data/graphs/c_alias/fs.g,data/grammars/c_alias.cnf",
+                        "data/graphs/c_alias/ipc.g,data/grammars/c_alias.cnf",
+                        "data/graphs/c_alias/lib.g,data/grammars/c_alias.cnf",
+                        "data/graphs/c_alias/mm.g,data/grammars/c_alias.cnf",
+                        "data/graphs/c_alias/net.g,data/grammars/c_alias.cnf",
+                        "data/graphs/c_alias/security.g,data/grammars/c_alias.cnf",
+                        "data/graphs/c_alias/sound.g,data/grammars/c_alias.cnf",
+                        "data/graphs/c_alias/arch.g,data/grammars/c_alias.cnf",
+                        "data/graphs/c_alias/crypto.g,data/grammars/c_alias.cnf",
+                        "data/graphs/c_alias/drivers.g,data/grammars/c_alias.cnf",
+                        "data/graphs/c_alias/kernel.g,data/grammars/c_alias.cnf",
+                        "data/graphs/c_alias/postgre.g,data/grammars/c_alias.cnf",
+                        "data/graphs/c_alias/apache.g,data/grammars/c_alias.cnf",
 
-"data/graphs/c_alias/init.g,data/grammars/c_alias.cnf",
-    "data/graphs/c_alias/block.g,data/grammars/c_alias.cnf",
-    "data/graphs/c_alias/fs.g,data/grammars/c_alias.cnf",
-    "data/graphs/c_alias/ipc.g,data/grammars/c_alias.cnf",
-    "data/graphs/c_alias/lib.g,data/grammars/c_alias.cnf",
-    "data/graphs/c_alias/mm.g,data/grammars/c_alias.cnf",
-    "data/graphs/c_alias/net.g,data/grammars/c_alias.cnf",
-    "data/graphs/c_alias/security.g,data/grammars/c_alias.cnf",
-    "data/graphs/c_alias/sound.g,data/grammars/c_alias.cnf",
-    "data/graphs/c_alias/arch.g,data/grammars/c_alias.cnf",
-    "data/graphs/c_alias/crypto.g,data/grammars/c_alias.cnf",
-    "data/graphs/c_alias/drivers.g,data/grammars/c_alias.cnf",
-    "data/graphs/c_alias/kernel.g,data/grammars/c_alias.cnf",
-    "data/graphs/c_alias/postgre.g,data/grammars/c_alias.cnf",
-    "data/graphs/c_alias/apache.g,data/grammars/c_alias.cnf",
+                        "data/graphs/vf/xz.g,data/grammars/vf.cnf",
+                        "data/graphs/vf/nab.g,data/grammars/vf.cnf",
+                        "data/graphs/vf/leela.g,data/grammars/vf.cnf", 
+                        
+                        "data/graphs/aa/cactus.g,data/grammars/aa.cnf",
+                        "data/graphs/aa/imagick.g,data/grammars/aa.cnf",
+                        "data/graphs/aa/leela.g,data/grammars/aa.cnf",
+                        "data/graphs/aa/nab.g,data/grammars/aa.cnf",
+                        "data/graphs/aa/omnetpp.g,data/grammars/aa.cnf",
+                        "data/graphs/aa/parest.g,data/grammars/aa.cnf",
+                        "data/graphs/aa/perlbench.g,data/grammars/aa.cnf",
+                        "data/graphs/aa/povray.g,data/grammars/aa.cnf",
+                        "data/graphs/aa/x264.g,data/grammars/aa.cnf",
+                        "data/graphs/aa/xz.g,data/grammars/aa.cnf",
+                        
+                        "data/graphs/rdf/go_hierarchy.g,data/grammars/"
+                        "nested_parentheses_subClassOf_type.cnf",
+                        "data/graphs/rdf/taxonomy.g,data/grammars/"
+                        "nested_parentheses_subClassOf_type.cnf",
+                        "data/graphs/rdf/eclass.g,data/grammars/"
+                        "nested_parentheses_subClassOf_type.cnf",
+                        "data/graphs/rdf/go.g,data/grammars/"
+                        "nested_parentheses_subClassOf_type.cnf",
+                        "data/graphs/rdf/taxonomy_hierarchy.g,data/grammars/"
+                        "nested_parentheses_subClassOf_type.cnf",
 
-                       "data/graphs/vf/xz.g,data/grammars/vf.cnf",
-                      "data/graphs/vf/nab.g,data/grammars/vf.cnf",
-                      "data/graphs/vf/leela.g,data/grammars/vf.cnf", 
-                      
-                      "data/graphs/aa/cactus.g,data/grammars/aa.cnf",
-                      "data/graphs/aa/imagick.g,data/grammars/aa.cnf",
-                      "data/graphs/aa/leela.g,data/grammars/aa.cnf",
-                      "data/graphs/aa/nab.g,data/grammars/aa.cnf",
-                      "data/graphs/aa/omnetpp.g,data/grammars/aa.cnf",
-                      "data/graphs/aa/parest.g,data/grammars/aa.cnf",
-                      "data/graphs/aa/perlbench.g,data/grammars/aa.cnf",
-                      "data/graphs/aa/povray.g,data/grammars/aa.cnf",
-                      "data/graphs/aa/x264.g,data/grammars/aa.cnf",
-                      "data/graphs/aa/xz.g,data/grammars/aa.cnf",
-                      
                       NULL};
 
 char *configs_my[] = {
     "data/graphs/java/sunflow.g,data/grammars/java_points_to.cnf",
     NULL};
-
-
-// Number of benchmark runs on a single graph
-#define COUNT 1
-// If true, the first run is done without measuring time (warm-up)
-#define HOT false
-// Use your custom configuration for the benchmark (default is the xz.g graph
-// and vf.cnf grammar)
-#define configs configs_aa
 
 int main(int argc, char **argv) {
     printf("LAGraph_CFL_reachability:\n");
