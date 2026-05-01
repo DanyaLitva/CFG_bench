3077f4e5b95a4ec64ef82d2f9ea251c461497e1f
Litvyakov Daniil <140255125+DanyaLitva@users.noreply.github.com>
2026-05-02 00:45:24 +0300

    add all paths adv
    оптимизации включаются так же, как и у достижимости

--- src/test.c
+++ src/test.c
@@ -3,6 +3,7 @@
 #include "adapter_CFL_adv.h"
 #include "adapter_CFL_all_path.h"
 #include "adapter_CFL_single_path.h"
+#include "adapter_CFL_all_path_adv.h"
 #include "memory.h"
 #include "parser.h"
 #include "result_manager.h"
@@ -105,7 +106,7 @@ static void print_usage(const char *program_name) {
             "  -r <rounds>       Number of benchmark rounds (default: 10)\n"
             "  --hot             Enable HOT launch (warm-up run before measurements)\n"
             "  -a <algorithm>    Algorithm to use "
-            "(default: CFL_adv; options: CFL_adv, CFL, CFL_single_path, CFL_all_path)\n"
+            "(default: CFL_adv; options: CFL_adv, CFL, CFL_single_path, CFL_all_path, CFL_all_path_adv)\n"
             "\n"
             "Optimization flags:\n"
             "  -e                Enable empty optimization\n"
@@ -189,6 +190,8 @@ int main(int argc, char **argv) {
                 adapter = adapter_CFL_all_paths_get_methods();
             } else if (strcmp(algo, "CFL_CFPQ_RSM") == 0) {
                 adapter = adapter_CFL_CFPQ_RSM_get_methods();
+            } else if (strcmp(algo, "CFL_all_path_adv") == 0) {
+                adapter = adapter_CFL_all_path_adv_get_methods();
             } else {
                 fprintf(stderr, "Unknown algorithm: %s\n", algo);
                 exit(EXIT_FAILURE);
